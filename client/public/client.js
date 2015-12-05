9e6fdcfc87a3675d6b9ef7e4d25d218539d02e08
brankeye <brankeye@gmail.com>
2015-12-05 17:19:33 -0500

    Fixed client options button group.

--- client/public/client.js
+++ client/public/client.js
@@ -1,5 +1,21 @@
 var $ = global.jQuery;
 
 $(document).ready(function() {
+  $('.btn-group #status').click(function() {
+    console.log("Set status.");
+    $(this).blur();
+  });
+  $('.btn-group #mic').click(function() {
+    console.log("Toggle mic.");
+    $(this).blur();
+  });
+  $('.btn-group #speakers').click(function() {
+    console.log("Toggle speakers.");
+    $(this).blur();
+  });
+  $('.btn-group #contacts').click(function() {
+    console.log("Get contacts.");
+    $(this).blur();
+  });
 });
 
