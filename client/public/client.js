2c8782c65a86aa4dc1b162db3b6d71a20a0712c7
brankeye <brankeye@gmail.com>
2015-12-06 19:00:56 -0500

    Added server connect option, not fully implemented.

--- client/public/client.js
+++ client/public/client.js
@@ -6,6 +6,7 @@ var $ = global.jQuery;
 $(document).ready(function() {
   
   // Initial setup
+  initAppMenu();
   loadClientList();
   handleMessagePane();
   
@@ -40,7 +41,7 @@ function handleMessagePane() {
 }
 
 function getMessage(user, msg) {
-  var messagePane = $('.active > .well#message-pane');
+  var messagePane = $('.tab-pane.active > .well#message-pane');
   messagePane.append('<p><strong><a href="#user" id="user">' + user + '</a>:</strong> ' + msg + '</p>');
   var paneHeight = messagePane.prop('scrollHeight');
   messagePane.scrollTop(paneHeight);
@@ -93,4 +94,142 @@ function handleUserContextMenu(username, event) {
   menu.append(new MenuItem({ label: 'Poke', click: function() { console.log('Poking ' + username + '.'); }}));
   event.preventDefault();
   menu.popup(remote.getCurrentWindow());
+}
+
+function initAppMenu() {
+  var template = 
+  [
+    {
+      label: 'Connections',
+      submenu: [
+        {
+          label: 'Connect',
+          accelerator: 'CmdOrCtrl+Shift+C',
+          click: function() {
+            console.log('Connect to a server.');
+          }
+        },
+        {
+          label: 'Disconnect',
+          accelerator: 'CmdOrCtrl+Shift+D',
+          click: function() {
+            console.log('Disconnect from a server.');
+          }
+        },
+        {
+          label: 'Server List',
+          accelerator: 'CmdOrCtrl+Shift+S',
+          click: function() {
+            console.log('Get the server list.');
+          }
+        },
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Ctrl+Command+F';
+            else
+              return 'F11';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Alt+Command+I';
+            else
+              return 'Ctrl+Shift+I';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.toggleDevTools();
+          }
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+      ]
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: function() { require('electron').shell.openExternal('http://electron.atom.io') }
+        },
+      ]
+    },
+  ];
+
+  var menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
